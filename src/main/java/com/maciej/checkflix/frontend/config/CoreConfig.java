f18563381d97a1e75f03cc7793a46550f722e008
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-12-04 08:43:04 +1100

    WIP Review Analytics view

--- src/main/java/com/maciej/checkflix/frontend/config/CoreConfig.java
+++ src/main/java/com/maciej/checkflix/frontend/config/CoreConfig.java
@@ -2,7 +2,12 @@ package com.maciej.checkflix.frontend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class CoreConfig {
@@ -10,5 +15,20 @@ public class CoreConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+        ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+        arrayHttpMessageConverter.setSupportedMediaTypes(getSupportedMediaTypes());
+        return arrayHttpMessageConverter;
+    }
+
+    private List<MediaType> getSupportedMediaTypes() {
+        List<MediaType> list = new ArrayList<MediaType>();
+        list.add(MediaType.IMAGE_JPEG);
+        list.add(MediaType.IMAGE_PNG);
+        list.add(MediaType.APPLICATION_OCTET_STREAM);
+        return list;
     }
 }
--- src/main/java/com/maciej/checkflix/frontend/ui/reviewsanalysisview/ReviewsAnalysisView.java
+++ src/main/java/com/maciej/checkflix/frontend/ui/reviewsanalysisview/ReviewsAnalysisView.java
@@ -1,19 +1,47 @@
-package com.maciej.checkflix.frontend.ui.watchlistview;
+package com.maciej.checkflix.frontend.ui.reviewsanalysisview;
 
 import com.maciej.checkflix.frontend.service.BackEndService;
 import com.maciej.checkflix.frontend.ui.AbstractMovieView;
 import com.maciej.checkflix.frontend.ui.MainLayout;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.OptionalParameter;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PageConfigurator;
+import com.vaadin.flow.server.StreamResource;
 
-@Route(value = "watchlist", layout = MainLayout.class)
-public class WatchListView extends AbstractMovieView {
+import java.io.ByteArrayInputStream;
 
-    public WatchListView(BackEndService backEndService) {
+@Route(value = "reviews_analysis", layout = MainLayout.class)
+public class ReviewsAnalysisView extends AbstractMovieView {
+
+    public ReviewsAnalysisView(BackEndService backEndService) {
         super(backEndService);
-
-        add(new H1("Watchlist"));
     }
 
+    @Override
+    public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
+        super.setParameter(event, parameter);
+
+        setUpHeader();
+
+        addWordCloud();
+    }
+
+    private void setUpHeader() {
+        add(new H1("Reviews analysis - " + movieTitle));
+    }
+
+    private void addWordCloud() {
+        StreamResource resource = new StreamResource(
+                "wordcloud.png",
+                () -> new ByteArrayInputStream(backEndService.getWordCloudForReviewsOf(movieImdbId)));
+        Image image = new Image(resource, "wordcloud");
+
+        add(image);
+    }
 }
